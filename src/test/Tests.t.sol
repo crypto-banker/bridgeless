fa38cf7c4e0e0824d07dbdb9755000433841c8fe
ChaoticWalrus <80983173+crypto-banker@users.noreply.github.com>
2022-08-15 22:24:16 -0700

    tests passing on 3 networks, fuzzing working

--- src/test/Tests.t.sol
+++ src/test/Tests.t.sol
@@ -22,8 +22,17 @@ contract Tests is Test {
     // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)")
     bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
 
-    address internal constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address internal constant ETH_USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address internal constant ETH_WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    address internal constant POLYGON_USDC = 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174;
+    address internal constant WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
+
+    address internal constant ARBI_USDC = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
+    address internal constant ARBI_WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
+
+    uint256 internal constant MAX_BIPS = 10000;
+    uint256 internal constant MAX_FEE_BIPS = 9000;
 
     uint256 _amountIn;
     uint256 _amountOutMin;
@@ -33,7 +42,27 @@ contract Tests is Test {
     function setUp() public {
     }
 
-    function testGaslessSwap() public {
+    function testGaslessSwapMainnet() public {
+        uint256 forkId = cheats.createFork("mainnet");
+        cheats.selectFork(forkId);
+        _testGaslessSwap(5555);
+    }
+
+    function testGaslessSwapPolygon() public {
+        uint256 forkId = cheats.createFork("polygon");
+        cheats.selectFork(forkId);
+        _testGaslessSwap(2500);
+    }
+
+    function testGaslessSwapArbitrum(uint16 feeBips) public {
+        uint256 forkId = cheats.createFork("arbitrum");
+        cheats.selectFork(forkId);
+        _testGaslessSwap(feeBips);
+    }
+
+    function _testGaslessSwap(uint16 _feeBips) internal {
+        cheats.assume(_feeBips <= MAX_FEE_BIPS);
+
         // initialize memory structs
         Bridgeless.UniswapOrder memory uniswapOrder;
         Bridgeless.Permit memory permit;
@@ -42,25 +71,76 @@ contract Tests is Test {
         uint256 chainId = block.chainid;
         // emit the chainId for logging purposes
         emit log_named_uint("chainId", chainId);
+
         // for testing on forked ETH mainnet
         if (chainId == 1) {
-            // swap 1e6 USDC for at least 1e9 ETH (i.e. one full USDC for at least one gwei)
+            // swap 1e6 ETH_USDC for at least 1e9 ETH (i.e. one full ETH_USDC for at least one gwei)
             _amountIn = 1e6;
             _amountOutMin = 1e9;
 
+            // uniswap router
             ROUTER = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
             FACTORY = IUniswapV2Factory(ROUTER.factory());
 
-            // set path to swap USDC => ETH
+            // set path to swap ETH_USDC => ETH
             address[] memory _path = new address[](2);
-            // USDC
-            _path[0] = USDC;
-            // canonical WETH
-            _path[1] = WETH;
+            // ETH_USDC
+            _path[0] = ETH_USDC;
+            // canonical ETH_WETH
+            _path[1] = ETH_WETH;
             uniswapOrder.path = _path;
 
             // binance bridge
             addressToSendTokenFrom = 0x47ac0Fb4F2D84898e4D9E7b4DaB3C24507a6D503;
+        }
+
+        // for testing on forked polygon mainnet
+        else if (chainId == 137) {
+            // swap 1e6 POLYGON_USDC for at least 1e9 WMATIC (i.e. one full POLYGON_USDC for at least one gwei)
+            _amountIn = 1e6;
+            _amountOutMin = 1e9;
+
+            // quickswap router
+            ROUTER = IUniswapV2Router02(0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff);
+            FACTORY = IUniswapV2Factory(ROUTER.factory());
+
+            // set path to swap POLYGON_USDC => WMATIC
+            address[] memory _path = new address[](2);
+            // POLYGON_USDC
+            _path[0] = POLYGON_USDC;
+            // canonical WMATIC
+            _path[1] = WMATIC;
+            uniswapOrder.path = _path;
+
+            // AAVE address
+            addressToSendTokenFrom = 0x625E7708f30cA75bfd92586e17077590C60eb4cD;
+        }
+
+        // for testing on forked arbitrum
+        else if (chainId == 42161) {
+            // swap 1e6 ARBI_USDC for at least 1e9 ARBI_WETH (i.e. one full ARBI_USDC for at least one gwei)
+            _amountIn = 1e6;
+            _amountOutMin = 1e9;
+
+            // SushiSwap router
+            ROUTER = IUniswapV2Router02(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506);
+            FACTORY = IUniswapV2Factory(ROUTER.factory());
+
+            // set path to swap ARBI_USDC => ARBI_WETH
+            address[] memory _path = new address[](2);
+            // ARBI_USDC
+            _path[0] = ARBI_USDC;
+            // canonical ARBI_WETH
+            _path[1] = ARBI_WETH;
+            uniswapOrder.path = _path;
+
+            // some bridge / exchange ? found through arbiscan
+            addressToSendTokenFrom = 0x1714400FF23dB4aF24F9fd64e7039e6597f18C2b;
+        }
+
+        else {
+            emit log_named_uint("ERROR: unsupported chainId number", chainId);
+            revert("support for chain not yet added to *tests* -- you can still launch on the chain though!");
         }
 
         uniswapOrder.amountIn = _amountIn;
@@ -70,7 +150,7 @@ contract Tests is Test {
         uniswapOrder.deadline = type(uint256).max;
 
         // set fee at 10%
-        uniswapOrder.feeBips = 1000;
+        uniswapOrder.feeBips = _feeBips;
 
         // deploy the Bridgeless contract
         bridgeless = new Bridgeless(ROUTER);
@@ -127,14 +207,17 @@ contract Tests is Test {
         cheats.stopPrank();
 
         // log address balances
-        emit log_named_uint("user.balance", user.balance);
-        emit log_named_uint("submitter.balance", submitter.balance);
-        // I'm fairly certain this will fail on a flaky basis due to rounding, but it seemed fun to add
-        assertEq(
-            user.balance * uniswapOrder.feeBips / (10000 - uniswapOrder.feeBips),
-            submitter.balance,
-            "bad fee charge"
-        );
+        uint256 userBalance = user.balance;
+        uint256 submitterBalance = submitter.balance;
+        emit log_named_uint("userBalance", userBalance);
+        emit log_named_uint("submitterBalance", submitterBalance);
+        uint256 calculatedFeeBips;
+        if (submitterBalance > 0) {
+            // we add (submitterBalance - 1) in the first part of this calculation here to counter-act rounding down when the fee is actually calculated
+            calculatedFeeBips = (submitterBalance * MAX_BIPS + submitterBalance - 1) / (submitterBalance + userBalance);
+        }
+        emit log_named_uint("calculatedFeeBips (backed out)", calculatedFeeBips);
+        require(calculatedFeeBips == uniswapOrder.feeBips, "fee bips incorrect!");
     }
 
     // unused internal function, left in from prior testing
