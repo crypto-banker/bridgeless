869cad7d132f85fa1961062b0c465c4771a289f1
CosmicTomato <80983173+crypto-banker@users.noreply.github.com>
2022-08-20 18:41:08 -0700

    Separate out some signature functionality
    can probably split this further into a library, but for a start I'm creating the abstract `BridgelessOrderSignatures` contract

--- src/contracts/Bridgeless.sol
+++ src/contracts/Bridgeless.sol
@@ -5,47 +5,18 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-import "./BridgelessStructs.sol";
+import "./BridgelessOrderSignatures.sol";
 import "./interfaces/IBridgelessCallee.sol";
 
 import "forge-std/Test.sol";
 
 contract Bridgeless is
-    BridgelessStructs,
+    BridgelessOrderSignatures,
     ReentrancyGuard
-    ,DSTest
+    // ,DSTest
 {
-    Vm cheats = Vm(HEVM_ADDRESS);
+    // Vm cheats = Vm(HEVM_ADDRESS);
     using SafeERC20 for IERC20;
-    /// @notice The EIP-712 typehash for the contract's domain
-    // bytes32 public constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract");
-    bytes32 public constant DOMAIN_TYPEHASH = 0x4e2c4bf03f58b0b9d87019acd26e490aca9f5097fac5fd3eed5cccf6342a8d85;
-
-    // struct BridgelessOrder_Base {
-    //     // ERC20 token to trade
-    //     address tokenIn;
-    //     // amount of token to trade
-    //     uint256 amountIn;
-    //     // desired token to trade into
-    //     address tokenOut;
-    //     // minimum amount of native token to receive
-    //     uint256 amountOutMin;
-    //     // signature expiration
-    //     uint256 deadline;
-    // }
-    /// @notice The EIP-712 typehash for the `ORDER_TYPEHASH_Base` order struct used by the contract
-    bytes32 public constant ORDER_TYPEHASH_Base = keccak256(
-        "BridgelessOrder_Base(address tokenIn,uint256 amountIn, address tokenOut,uint256 amountOutMin,uint256 deadline)");
-
-    /// @notice The EIP-712 typehash for the `ORDER_TYPEHASH_Base` order struct used by the contract
-    bytes32 public constant ORDER_TYPEHASH_WithNonce = keccak256(
-        "BridgelessOrder_WithNonce(BridgelessOrder_Base orderBase,uint256 nonce)");
-
-    /// @notice EIP-712 Domain separator
-    bytes32 public immutable DOMAIN_SEPARATOR;
-
-    // signer => nonce => whether or not the nonce has been spent already
-    mapping(address => mapping(uint256 => bool)) public nonceIsSpent;
 
     // verify that the `tokenOwner` receives *at least* `amountOutMin in `tokenOut` from the swap
     modifier checkOrderFulfillment(
@@ -73,15 +44,6 @@ contract Bridgeless is
             "Bridgeless.checkOrderDeadline: block.timestamp > deadline"
         );
         _;
-    }
-
-    // set immutable variables
-    constructor()
-    {
-        // initialize the immutable DOMAIN_SEPARATOR for signatures
-        DOMAIN_SEPARATOR = keccak256(
-            abi.encode(DOMAIN_TYPEHASH, bytes("Bridgeless"), block.chainid, address(this))
-        );
     }
 
     /**
@@ -268,91 +230,6 @@ contract Bridgeless is
             }
         }
     }
-
-    /**
-     * @notice Simple getter function to calculate the `orderHash` for a `BridgelessOrder_Base`
-     * @param orderBase A `BridgelessOrder_Base` ojbect
-     */
-    function calculateBridgelessOrderHash_Base(BridgelessOrder_Base calldata orderBase) public pure returns (bytes32) {
-        return(
-            keccak256(
-                abi.encode(
-                    ORDER_TYPEHASH_Base,
-                    orderBase.tokenIn,
-                    orderBase.amountIn,
-                    orderBase.tokenOut,
-                    orderBase.amountOutMin,
-                    orderBase.deadline
-                )
-            )
-        );
-    }
-
-    /**
-     * @notice Simple getter function to calculate the `orderHash` for a `BridgelessOrder_Simple`
-     * @param order A `BridgelessOrder_Simple`-type order
-     */
-    function calculateBridgelessOrderHash_Simple(BridgelessOrder_Simple calldata order) public pure returns (bytes32) {
-        return calculateBridgelessOrderHash_Base(order.orderBase);
-    }
-
-    /**
-     * @notice Simple getter function to calculate the `orderHash` for a `BridgelessOrder_WithNonce`
-     * @param order A `BridgelessOrder_WithNonce`-type order
-     */
-    function calculateBridgelessOrderHash_WithNonce(BridgelessOrder_WithNonce calldata order) public pure returns (bytes32) {
-        return(
-            keccak256(
-                abi.encode(
-                    ORDER_TYPEHASH_WithNonce,
-                    calculateBridgelessOrderHash_Base(order.orderBase),
-                    order.nonce
-                )
-            )
-        );
-    }
-
-    function _checkOrderSignature_Simple(address signer, BridgelessOrder_Simple calldata order, Signature calldata signature) internal pure {
-        // verify the order signature
-        require(
-            signer == ECDSA.recover(
-                // calculate the orderHash
-                calculateBridgelessOrderHash_Simple(order),
-                signature.v,
-                signature.r,
-                signature.s
-            ),
-            "Bridgeless._checkOrderSignature_Simple: signer != recoveredAddress"
-        );
-    }
-
-    function _checkOrderSignature_WithNonce(address signer, BridgelessOrder_WithNonce calldata order, Signature calldata signature) internal {
-        // check nonce validity
-        if (nonceIsSpent[signer][order.nonce]) {
-            revert("Bridgeless._checkOrderSignature_WithNonce: nonce is already spent");
-        }
-        // mark nonce as spent
-        nonceIsSpent[signer][order.nonce] = true;
-        // verify the order signature
-        require(
-            signer == ECDSA.recover(
-                // calculate the orderHash
-                calculateBridgelessOrderHash_WithNonce(order),
-                signature.v,
-                signature.r,
-                signature.s
-            ),
-            "Bridgeless._checkOrderSignature_WithNonce: signer != recoveredAddress"
-        );
-    }
-
-    function _checkOrderDeadline(uint256 deadline) internal view {
-        require(
-            block.timestamp <= deadline,
-            "Bridgeless._checkOrderDeadline: block.timestamp > deadline"
-        );
-    }
-
 
     // fetches the `user`'s balance of `token`, where `token == address(0)` indicates the chain's native token
     function _getUserBalance(address user, address token) internal view returns (uint256) {
---
+++ src/contracts/BridgelessOrderSignatures.sol
@@ -0,0 +1,138 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.12;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "./BridgelessStructs.sol";
+
+import "forge-std/Test.sol";
+
+abstract contract BridgelessOrderSignatures is
+    BridgelessStructs
+    ,DSTest
+{
+    using SafeERC20 for IERC20;
+    /// @notice The EIP-712 typehash for the contract's domain
+    // bytes32 public constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract");
+    bytes32 public constant DOMAIN_TYPEHASH = 0x4e2c4bf03f58b0b9d87019acd26e490aca9f5097fac5fd3eed5cccf6342a8d85;
+
+    // struct BridgelessOrder_Base {
+    //     // ERC20 token to trade
+    //     address tokenIn;
+    //     // amount of token to trade
+    //     uint256 amountIn;
+    //     // desired token to trade into
+    //     address tokenOut;
+    //     // minimum amount of native token to receive
+    //     uint256 amountOutMin;
+    //     // signature expiration
+    //     uint256 deadline;
+    // }
+    /// @notice The EIP-712 typehash for the `ORDER_TYPEHASH_Base` order struct used by the contract
+    bytes32 public constant ORDER_TYPEHASH_Base = keccak256(
+        "BridgelessOrder_Base(address tokenIn,uint256 amountIn, address tokenOut,uint256 amountOutMin,uint256 deadline)");
+
+    /// @notice The EIP-712 typehash for the `ORDER_TYPEHASH_Base` order struct used by the contract
+    bytes32 public constant ORDER_TYPEHASH_WithNonce = keccak256(
+        "BridgelessOrder_WithNonce(BridgelessOrder_Base orderBase,uint256 nonce)");
+
+    /// @notice EIP-712 Domain separator
+    bytes32 public immutable DOMAIN_SEPARATOR;
+
+    // signer => nonce => whether or not the nonce has been spent already
+    mapping(address => mapping(uint256 => bool)) public nonceIsSpent;
+
+    // set immutable variables
+    constructor()
+    {
+        // initialize the immutable DOMAIN_SEPARATOR for signatures
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(DOMAIN_TYPEHASH, bytes("Bridgeless"), block.chainid, address(this))
+        );
+
+    }
+
+    /**
+     * @notice Simple getter function to calculate the `orderHash` for a `BridgelessOrder_Simple`
+     * @param order A `BridgelessOrder_Simple`-type order
+     */
+    function calculateBridgelessOrderHash_Simple(BridgelessOrder_Simple calldata order) public pure returns (bytes32) {
+        return calculateBridgelessOrderHash_Base(order.orderBase);
+    }
+
+    /**
+     * @notice Simple getter function to calculate the `orderHash` for a `BridgelessOrder_Base`
+     * @param orderBase A `BridgelessOrder_Base` ojbect
+     */
+    function calculateBridgelessOrderHash_Base(BridgelessOrder_Base calldata orderBase) public pure returns (bytes32) {
+        return(
+            keccak256(
+                abi.encode(
+                    ORDER_TYPEHASH_Base,
+                    orderBase.tokenIn,
+                    orderBase.amountIn,
+                    orderBase.tokenOut,
+                    orderBase.amountOutMin,
+                    orderBase.deadline
+                )
+            )
+        );
+    }
+
+    /**
+     * @notice Simple getter function to calculate the `orderHash` for a `BridgelessOrder_WithNonce`
+     * @param order A `BridgelessOrder_WithNonce`-type order
+     */
+    function calculateBridgelessOrderHash_WithNonce(BridgelessOrder_WithNonce calldata order) public pure returns (bytes32) {
+        return(
+            keccak256(
+                abi.encode(
+                    ORDER_TYPEHASH_WithNonce,
+                    calculateBridgelessOrderHash_Base(order.orderBase),
+                    order.nonce
+                )
+            )
+        );
+    }
+
+    function _checkOrderSignature_Simple(address signer, BridgelessOrder_Simple calldata order, Signature calldata signature) internal pure {
+        // verify the order signature
+        require(
+            signer == ECDSA.recover(
+                // calculate the orderHash
+                calculateBridgelessOrderHash_Simple(order),
+                signature.v,
+                signature.r,
+                signature.s
+            ),
+            "Bridgeless._checkOrderSignature_Simple: signer != recoveredAddress"
+        );
+    }
+
+    function _checkOrderSignature_WithNonce(address signer, BridgelessOrder_WithNonce calldata order, Signature calldata signature) internal {
+        // check nonce validity
+        if (nonceIsSpent[signer][order.nonce]) {
+            revert("Bridgeless._checkOrderSignature_WithNonce: nonce is already spent");
+        }
+        // mark nonce as spent
+        nonceIsSpent[signer][order.nonce] = true;
+        // verify the order signature
+        require(
+            signer == ECDSA.recover(
+                // calculate the orderHash
+                calculateBridgelessOrderHash_WithNonce(order),
+                signature.v,
+                signature.r,
+                signature.s
+            ),
+            "Bridgeless._checkOrderSignature_WithNonce: signer != recoveredAddress"
+        );
+    }
+
+    function _checkOrderDeadline(uint256 deadline) internal view {
+        require(
+            block.timestamp <= deadline,
+            "Bridgeless._checkOrderDeadline: block.timestamp > deadline"
+        );
+    }
+}
